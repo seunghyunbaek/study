0f7c514a4f94144f2f6795361ea1f2ff1f76e832
Baek <Baek@Baek-PC>
2018-09-26 17:52:20 +0900

    Training JDBC with MySQL

---
+++ edwidth/Full-Stack Web Developer/Part2/jdbcexam0/src/main/java/kr/or/connect/jdbcexam0/JDBCExam0.java
@@ -0,0 +1,14 @@
+package kr.or.connect.jdbcexam0;
+
+import kr.or.connect.jdbcexam0.dao.RoleDao;
+import kr.or.connect.jdbcexam0.dto.Role;
+
+public class JDBCExam0 {
+
+	public static void main(String[] args) {
+		RoleDao dao = new RoleDao();
+		Role role = dao.getRole(100);
+		System.out.println(role);
+	}
+
+}
